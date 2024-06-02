6d9dc7ae59ae9b2291287949f66fbe5a3eb2a921
kyawhtetaung <kyawhtetaungdev14@gmail.com>
2024-06-02 20:39:55 +0630

    uibuild

--- index.js
+++ index.js
@@ -34,14 +34,6 @@ app.get('/api/notes/:id', (req, res) => {
     })
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-
-    return maxId + 1
-}
-
 app.post('/api/notes', (req, res) => {
     const body = req.body
 
